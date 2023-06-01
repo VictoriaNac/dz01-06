14b58f0bb3491ed136a9b33f4a27a93ce66b5518
Viktoria <android1986mir@rambler.ru>
2023-06-01 16:40:52 +0300

    36

--- 36.py
+++ 36.py
@@ -9,3 +9,6 @@
 # Sample Output:
 # (('house', 'дом'), ('car','машина'), ('men', 'человек'), ('tree', 'дерево'))
 
+s = input() 
+s_lst = s.split() 
+tp = tuple(map(lambda x: tuple(x.split('=')), s_lst))
